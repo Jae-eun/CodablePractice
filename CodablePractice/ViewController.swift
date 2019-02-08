6e2fe4862aa37813e8b49a8ccb9d7bc0b00ed712
Jae-eun <je4297@naver.com>
2019-02-09 02:54:13 +0900

    뷰컨에 적용

--- CodablePractice/ViewController.swift
+++ CodablePractice/ViewController.swift
@@ -18,8 +18,33 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    requestFeedbacks()
+    setLabel()
   }
 
-
+  var dustFeedback: DustFeedback?
+  let path = Bundle.main.path(forResource: "DustFeedback", ofType: "json")
+  
+  func requestFeedbacks() {
+    let jsonDecoder: JSONDecoder = JSONDecoder()
+    if let path = path {
+      do {
+        guard let data = try String(contentsOfFile: path).data(using: .utf8) else { return }
+        let dustFeedbacks = try jsonDecoder.decode([DustFeedback].self, from: data)
+        titleLabel.text = dustFeedbacks[0].title
+        authorLabel.text = dustFeedbacks[0].author
+        dateLabel.text = dustFeedbacks[0].date
+        contentsLabel.text = dustFeedbacks[0].contents
+      } catch {
+        print("error: \(error)")
+      }
+    }
+  }
+  
+  func setLabel() {
+//    titleLabel.text = dustFeedback[0].title
+//    authorLabel.text = dustFeedback[0].author
+//
+  }
 }
 
