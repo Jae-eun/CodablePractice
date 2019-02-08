0a627bc8bd3306630c2690ddc65342e8b53af984
Jae-eun <je4297@naver.com>
2019-02-09 04:15:11 +0900

    VC 수정, notification 추가

--- CodablePractice/ViewController.swift
+++ CodablePractice/ViewController.swift
@@ -9,42 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var authorLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
   @IBOutlet weak var contentsLabel: UILabel!
+  
+  var dustFeedbacks: [DustFeedback] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    requestFeedbacks()
-    setLabel()
   }
-
-  var dustFeedback: DustFeedback?
-  let path = Bundle.main.path(forResource: "DustFeedback", ofType: "json")
   
-  func requestFeedbacks() {
-    let jsonDecoder: JSONDecoder = JSONDecoder()
-    if let path = path {
-      do {
-        guard let data = try String(contentsOfFile: path).data(using: .utf8) else { return }
-        let dustFeedbacks = try jsonDecoder.decode([DustFeedback].self, from: data)
-        titleLabel.text = dustFeedbacks[0].title
-        authorLabel.text = dustFeedbacks[0].author
-        dateLabel.text = dustFeedbacks[0].date
-        contentsLabel.text = dustFeedbacks[0].contents
-      } catch {
-        print("error: \(error)")
-      }
-    }
+  override func viewDidAppear(_ animated: Bool) {
+    NotificationCenter.default.addObserver(self, selector: #selector(didReceiveFeedbackNotification(_:)), name: DidReceiveFeedbackNotification, object: nil)
+        requestFeedbacks()
+        setLabel()
+  }
+  
+  @objc func didReceiveFeedbackNotification(_ noti: Notification) {
+    guard let dustFeedbacks = noti.userInfo?["feedbacks"] as? [DustFeedback]
+      else { return }
+    
+    self.dustFeedbacks = dustFeedbacks
   }
   
   func setLabel() {
-//    titleLabel.text = dustFeedback[0].title
-//    authorLabel.text = dustFeedback[0].author
-//
+    titleLabel.text = dustFeedbacks[0].title
+    authorLabel.text = dustFeedbacks[0].author
+    dateLabel.text = dustFeedbacks[0].date
+    contentsLabel.text = dustFeedbacks[0].contents
   }
 }
 
