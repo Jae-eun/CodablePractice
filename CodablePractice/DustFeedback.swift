e21c387c6bdd24d9f4437b68681538fab7cdbffe
Jae-eun <je4297@naver.com>
2019-02-09 02:53:49 +0900

    구조체 만듦

---
+++ CodablePractice/DustFeedback.swift
@@ -0,0 +1,19 @@
+//
+//  DustFeedback.swift
+//  CodablePractice
+//
+//  Created by 이재은 on 08/02/2019.
+//  Copyright © 2019 이재은. All rights reserved.
+//
+
+import Foundation
+
+/// 'DustFeedback' json
+struct DustFeedback: Codable {
+  
+  let title: String
+  let author: String
+  let date: String
+  let contents: String
+  
+}
