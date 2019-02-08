ceed985af1bcdd5120bb68c187bdcda579cb9cab
Jae-eun <je4297@naver.com>
2019-02-08 16:00:00 +0900

    UILabel 만듦

--- CodablePractice/ViewController.swift
+++ CodablePractice/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var authorLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var contentsLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
